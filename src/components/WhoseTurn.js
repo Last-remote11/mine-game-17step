2f45543ceece2709b592e1c6d22084fb5ae5dca2
Sung Dong Kim <67620856+Last-remote11@users.noreply.github.com>
2021-05-11 23:21:50 +0900

    유국표시 문제있던 점 수정

--- src/components/WhoseTurn.js
+++ src/components/WhoseTurn.js
@@ -3,14 +3,14 @@ import { useSelector } from 'react-redux'
 
 const WhoseTurn = () => {
 
-  const { myTurn, soon } = useSelector(state => state.gameState)
-
+  const { myTurn, soon, opponentDiscards, myDiscards } = useSelector(state => state.gameState)
+  let howManyCards = opponentDiscards.length + myDiscards.length
   return (
     <div>
       {soon}순
       {
         myTurn
-        ? soon===18 ? <h2>17순이 끝났습니다. 아무 패나 누르면 유국이 됩니다.</h2> : <h2>내 차례입니다.</h2>
+        ? howManyCards === 34 ? <h2>17순이 끝났습니다. 아무 패나 누르면 유국이 됩니다.</h2> : <h2>내 차례입니다.</h2>
         : <h2>상대방의 차례입니다.</h2>
       }
     </div>
